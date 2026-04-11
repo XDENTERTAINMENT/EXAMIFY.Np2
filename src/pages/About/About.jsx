d1c46a5735e8517178355cd85f1e4d302c53678a
NJEZE <Njezepeter@gmail.com>
2026-04-11 14:10:05 +0100

    integrated googleclient and packaged the backend api

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,18 +3,11 @@ import React from 'react'
 function About() {
   return (
     <div>
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores minima vero magnam
-         explicabo! Fugiat, ipsam distinctio ea modi sint commodi repellat tempore maxime,
-          quae autem fugit eaque nemo amet animi officia, ad minima provident
-           sapiente placeat pariatur sunt! Tempora obcaecati eius neque quibusdam deserunt.
-            Nihil illum dicta, voluptatem officia deleniti fugit odio. 
-            Itaque atque natus porro asperiores aliquam, laborum totam,
-             tempora sunt perspiciatis soluta earum cumque voluptatum ipsum voluptatibus ullam eos! 
-             Corrupti modi molestiae ipsam cumque architecto consequuntur rem voluptatum doloribus 
-             necessitatibus a atque ea inventore velit deleniti maiores nam, unde vero quod mollitia?
-              Nesciunt id, illum provident qui eum temporibus architecto mollitia laborum odio vel iure fuga
-              , esse quam veniam maiores. Numquam cupiditate itaque accusantium quos maiores qui quaerat odit, dolorem veniam excepturi laborum, nisi id autem quibusdam natus aspernatur dignissimos! Odit magni voluptate et voluptatibus ab, vitae dolores neque reprehenderit, rem corrupti atque praesentium! Fugit, suscipit doloremque tenetur ut alias aut dicta fuga voluptatem aliquam, architecto exercitationem ex iure ad dolorem facere magni, impedit eos. Unde, quasi. Placeat id atque quam, dolor recusandae alias unde corrupti, et quasi sed impedit temporibus velit laborum, modi culpa numquam voluptatum
-         facilis error! Numquam rerum at quia nemo, suscipit nisi asperiores cumque!</p>
+       <p>Welcome to our Smart Exam Platform — a modern digital learning solution built to make online assessments simple, 
+        secure, and efficient 📚✨. Whether you're a student preparing for exams or an educator managing tests, 
+        our platform provides a seamless experience for creating, organizing, and taking assessments from anywhere.
+         With an intuitive interface, real-time feedback, and reliable performance,
+         we help transform traditional exams into a smarter, faster, and more engaging process. 🚀</p>
     </div>
   )
 }
