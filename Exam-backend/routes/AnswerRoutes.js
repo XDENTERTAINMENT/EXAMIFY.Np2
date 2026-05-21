7523f551f18f58a9250b62ad237d8397f7164f48
NJEZE <Njezepeter@gmail.com>
2026-05-21 14:08:26 +0100

    fix production cors

--- Exam-backend/routes/AnswerRoutes.js
+++ Exam-backend/routes/AnswerRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const answerController = require("../controllers/answerController.js");
+const answerController = require("../controllers/answerController");
 
 router.post("/save-answer", answerController.saveAnswer);
 
