6225637cf3de714baab5c73349a671e42b221887
NJEZE <Njezepeter@gmail.com>
2026-05-21 13:08:53 +0100

    updated

--- Exam-backend/server.js
+++ Exam-backend/server.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://examify-np2",
+      "https://examify-np2.vercel.app",
       "https://examifyedu.com",
       "https://www.examifyedu.com",
     ],
