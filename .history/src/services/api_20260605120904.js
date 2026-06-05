4dc71563e31790cdab82edd75b573a1b7f1ab4ef
NJEZE <Njezepeter@gmail.com>
2026-06-05 12:09:18 +0100

    updated api link

---
+++ .history/src/services/api_20260605120904.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: " https://exam-backend-examify.onrender.com/api",
+});
+
+
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+
+// ✅ ONE clean interceptor only
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+
+  // console.log("TOKEN:", token);
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
+});
+
+export default API;
