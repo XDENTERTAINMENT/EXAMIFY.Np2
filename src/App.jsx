cec8c6e906cb4ba0822616c4566a0b8e4c927a19
NJEZE <Njezepeter@gmail.com>
2026-06-29 07:43:57 +0100

    integrated paystack and subscript ui

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="head-office">
       <BrowserRouter>
-      <UpdateNotice/>
+      {/* <UpdateNotice/> */}
         <Navbar />
         <div className="routes-container">
           <AppRoutes />
